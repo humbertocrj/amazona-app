c94f0b6f4014a437e69537ee75afea3136201270
humberto r.junior <junior.humberto.ramos@gmail.com>
2022-02-22 09:41:19 -0300

    Creating user signin

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,21 @@ import mongoose from "mongoose";
 import data from "./data.js";
 import productRouter from "./routers/productRouter.js";
 import userRouter from "./routers/userRouter.js"
+import dotenv from 'dotenv'
+
+const res = dotenv.config()
+if(res.error){
+  throw res.error
+}
 const app = express();
 
 mongoose.connect(process.env.MONGODB_URL || 'mongodb://127.0.0.1:27017/amazona')
 
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
 app.use('/api/users', userRouter)
-app.use('/api/products', productRouter)
+app.use('/api/products', productRouter) 
 
 
 //Middleware used to get error in my routes (expressAsyncHandler
